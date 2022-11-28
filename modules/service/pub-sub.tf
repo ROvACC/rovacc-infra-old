f4388ee0ad53b28ac8de76cbb29133f2f9eb2299
George Enciu <george.enciu@gmail.com>
2022-11-29 00:16:54 +0100

    adds subscription with dql (test)

--- modules/service/pub-sub.tf
+++ modules/service/pub-sub.tf
@@ -7,3 +7,17 @@ resource "google_pubsub_topic" "rovacc_events" {
 
   message_retention_duration = "86600s"
 }
+
+resource "google_pubsub_topic" "rovacc_events_dlq" {
+  name = "${var.system_id}-${var.environment}-events-dlq"
+}
+
+resource "google_pubsub_subscription" "example" {
+  name  = "${var.system_id}-${var.environment}-events"
+  topic = google_pubsub_topic.rovacc_events.name
+
+  dead_letter_policy {
+    dead_letter_topic = google_pubsub_topic.rovacc_events_dlq.id
+    max_delivery_attempts = 10
+  }
+}
